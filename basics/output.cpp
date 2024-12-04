6da37c8fddf3dbf564a3ad9119632eb19f0cbf4e
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-12-04 11:27:27 -0500

    modified:output.cpp

---
+++ basics/output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    std::cout << "Nischal" <<std:: endl;
+    std::cout<< "Seemantula" << "\n";
+    std::cout << "University at Buffalo" << "\n" ;
+  
+
+// endl : is a newline character which flushes the output buffer flow whih more costly and slow
+// \n : It goes to a new line character with a escape sequence which low level and less cost more faster and efficient
+
+// Using namespace std
+
+    cout << "Hey I'm a sofware developer" << "\n";
+    cout << "I'm interested in ML domain too";
+    return 0;
+}
+
+
